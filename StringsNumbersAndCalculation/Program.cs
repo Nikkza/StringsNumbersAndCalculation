9760c373772f21ad178e56d475ff81e408df4816
Nikkza <isaksson.nicklas80@outlook.com>
2020-05-14 11:32:01 +0300

    Refactoring

--- StringsNumbersAndCalculation/Program.cs
+++ StringsNumbersAndCalculation/Program.cs
@@ -21,16 +21,16 @@ namespace StringsNumbersAndCalculation
 
         static string CalculateString(string calcI)
         {
-            List<double> results;
+           
             double numberOne = 0;
             double numberTwo = 0;
             var array = calcI.ToArray();
             double sum = 0;
             var op = array.Where(x => x == '*' || x == '-' || x == '+' || x == '/').FirstOrDefault();
-            results = ListAsChar(calcI, op);
-            numberOne = results[0];
-            numberTwo = results[1];
-         
+            var results = ListAsString(calcI, op);
+            numberOne = Converter(results[0]);
+            numberTwo = Converter(results[1]);
+
             switch (op.ToString())
             {
                 case "*":
@@ -52,15 +52,9 @@ namespace StringsNumbersAndCalculation
             return $"Sum: {Math.Round(sum)}";
         }
 
-        static List<double> ListAsChar(string splitToArray, char op)
-        {
-            var list = new List<double>();
-            var getNumbers = string.Join("", splitToArray.ToCharArray().Where( x => Char.IsDigit(x) || x == '*' || x == '-' || x == '+' || x == '/' || x == '.'));
-            var spliNumbers = getNumbers.Split(op);
-            list.Add(Converter(spliNumbers[0]));
-            list.Add(Converter(spliNumbers[1]));
-
-            return list;
+        static string[] ListAsString(string splitToArray, char op)
+        {            
+            return string.Join("", splitToArray.ToCharArray().Where( x => Char.IsDigit(x) || x == '*' || x == '-' || x == '+' || x == '/' || x == '.')).Split(op);      
         }
 
         static double Converter(string convertToDouble)
